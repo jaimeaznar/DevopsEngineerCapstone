3cc2e17ef79ad4e2360030da80fb722b477d1438
EC2 Default User <ec2-user@ip-172-31-40-213.ec2.internal>
2023-09-01 07:42:28 +0000

    Deploying

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def home():
     return "<h1 style='text-align: center;'>Jaime Aznar's DevOps Capstone!</h1>"
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
